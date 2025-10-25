f5c9b00a2904a3e4e008ba80cf8fdf9eb16583f0
shaurya1234524 <shaurya2312046@akgec.ac.in>
2025-10-25 12:20:19 +0530

    Add files via upload

---
+++ aixclone/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path,include
+from tools.views import ToolListView, submit_tool,signup_view,privacy_policy,terms_of_Service,cookie_policy
+from django.contrib import admin
+app_name = 'tools'
+from tools import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+
+urlpatterns = [
+        # ... your normal URLs ...
+     path('', ToolListView.as_view(), name='list'),
+    path('submit/', views.submit_tool, name='submit'),
+        path('sitemap.xml',views.sitemap,name="sitemap"),
+ path('robots.txt',views.robots,name="robots"),
+path('admin/',admin.site.urls),
+      path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+   path('signup/', signup_view, name='signup'),  # <-- add this
+    path('', include('tools.urls', namespace='tools')),
+    path('aboutus/',views.aboutus,name="aboutus"),
+      path('privacy_policy/',privacy_policy,name='privacy_policy'),
+      path('terms_of_service/',terms_of_Service,name='terms_of_service'),
+       path('cookie-policy/', views.cookie_policy, name='cookie_policy'),
+        path('9f4a2c1e8b7d6f3a2e1b4c9d0f6a7b8c.txt/',views.indexnow,name="indexnow"),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
